a2b843df28ced8deee852a0f749240b856970df5
tarikyavuzca <tarikyavuzca@gmail.com>
2021-09-23 15:01:20 -0400

    breedFetcher function created to fetch a bread from the cat API

---
+++ breedFetcher.js
@@ -0,0 +1,25 @@
+const request = require('request');
+// https://api.thecatapi.com/v1/images/search?=
+
+
+const fetchBreed = (breed, func) => {
+
+  let url = 'https://api.thecatapi.com/v1/breeds/search?q=';
+  request(url + breed, (err, response, body) => {
+    if (err) {
+      func(err, null);
+      return;
+    }
+    
+    let data = JSON.parse(body);
+    if (!data.length) {
+      func('Cat breed not found :(', null);
+      return;
+    }
+    
+    func(null, data[0].description);
+    
+  });
+};
+
+module.exports = fetchBreed ;
